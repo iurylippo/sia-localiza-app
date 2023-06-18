6c4cf8bec02401e6fa87cccbbc0b687724e4f5b1
iurylippo <iuryvieira.lippo@gmail.com>
2023-06-18 17:09:44 -0300

    feat: add port on env

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    'process.env': process.env
-  },
-  envDir: './',
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
-  base: './'
-})
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  if(!process.env.VITE_PORT) {
+    throw new Error('Port not found on env!');
+  }
+
+  if(!process.env.VITE_APP_API_URL) {
+    throw new Error('Api url not found on env!');
+  }
+
+  
+  return defineConfig({
+    define: {
+      'process.env': process.env
+    },
+    envDir: './',
+    server: {
+      port: Number(process.env.VITE_PORT)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+    base: './'
+  })
+}
